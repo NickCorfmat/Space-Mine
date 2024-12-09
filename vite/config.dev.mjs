ef35bb78326ce8f525f1a17f42b54e90a21c3be5
Nick Corfmat <ncorfmat@ucsc.edu>
2024-12-08 19:39:08 -0800

    adding favicons

--- vite/config.dev.mjs
+++ vite/config.dev.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
     base: './',
@@ -13,5 +14,31 @@ export default defineConfig({
     },
     server: {
         port: 8080
-    }
+    },
+    plugins: [
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'Your App Name',
+                short_name: 'AppName',
+                description: 'Your app description',
+                icons: [
+                    {
+                        src: 'icons/icon-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'icons/icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+                start_url: '/',
+                display: 'standalone',
+                theme_color: '#ffffff',
+                background_color: '#ffffff',
+            },
+        }),
+    ]
 });
