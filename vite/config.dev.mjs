6fced90f7be3450acca11133d6dd295ca28c0aba
Nick Corfmat <ncorfmat@ucsc.edu>
2024-12-08 20:30:57 -0800

    pwa working

--- vite/config.dev.mjs
+++ vite/config.dev.mjs
@@ -19,23 +19,43 @@ export default defineConfig({
         VitePWA({
             registerType: 'autoUpdate',
             manifest: {
-                name: 'Your App Name',
-                short_name: 'AppName',
-                description: 'Your app description',
+                name: 'Space-Mine',
+                short_name: 'SpaceMine',
+                description: 'A 2D mining simulator built using Phaser',
+                start_url: "index.html",
+                display: "fullscreen",
                 icons: [
                     {
-                        src: 'icons/icon-192x192.png',
+                        src: 'android-chrome-192x192.png',
                         sizes: '192x192',
                         type: 'image/png',
                     },
                     {
-                        src: 'icons/icon-512x512.png',
+                        src: 'android-chrome-512x512.png',
                         sizes: '512x512',
                         type: 'image/png',
                     },
+                    {
+                        src: 'apple-touch-icon.png',
+                        sizes: '180x180',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'favicon-16x16.png',
+                        sizes: '16x16',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'favicon-32x32.png',
+                        sizes: '32x32',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'favicon.ico',
+                        sizes: '48x48',
+                        type: 'image/x-icon',
+                    }
                 ],
-                start_url: '/',
-                display: 'standalone',
                 theme_color: '#ffffff',
                 background_color: '#ffffff',
             },
